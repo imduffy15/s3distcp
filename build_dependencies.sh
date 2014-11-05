27aefcb599204fd78f64042419e19d1c5449b668
Chris <christopher@fastestforward.com>
2014-11-05 14:52:56 -0500

    ensure we pull down the correct hadoop mapred jar as well

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e
 mkdir -p tmp/
-git clone https://github.com/electrum/hadoop-snappy.git tmp/hadoop-snappy
-cd tmp/hadoop-snappy && mvn install && cd -
+if [ ! -e tmp/hadoop-snappy ]; then
+  git clone https://github.com/electrum/hadoop-snappy.git tmp/hadoop-snappy
+  cd tmp/hadoop-snappy && mvn install && cd -
+fi
+if [ ! -e tmp/hadoop-mapred-0.22.0.jar ]; then
+  curl -o tmp/hadoop-mapred-0.22.0.jar http://central.maven.org/maven2/org/apache/hadoop/hadoop-mapred/0.22.0/hadoop-mapred-0.22.0.jar
+fi
+if [ ! -e tmp/hadoop-mapred-0.22.0.pom ]; then
+  curl -o tmp/hadoop-mapred-0.22.0.pom http://central.maven.org/maven2/org/apache/hadoop/hadoop-mapred/0.22.0/hadoop-mapred-0.22.0.pom
+fi
+cd tmp && mvn install:install-file -Dfile=hadoop-mapred-0.22.0.jar -DpomFile=hadoop-mapred-0.22.0.pom && cd -
 
